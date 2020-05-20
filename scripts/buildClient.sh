df4a6b91f51c80390ea4e629c855a3e9350da4db
Robin Wieschendorf <mail@robinwieschendorf.de>
2020-05-20 10:10:45 +0200

    feat: add buildClient.sh script

---
+++ scripts/buildClient.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# *** USAGE ***
+# Execute this script from the mmlc root directory
+# ./scripts/buildClient.sh
+
+# Prepare build directory
+mkdir ./build
+rm -rf ./build/ModifiedModuleLoaderClient.tar
+
+# Git
+git clone git@github.com:RobinTheHood/ModifiedModuleLoaderClient.git ./build/ModifiedModuleLoaderClient
+rm -rf ./build/ModifiedModuleLoaderClient/.git
+
+# Install dependencies
+composer install -d ./build/ModifiedModuleLoaderClient --no-dev
+composer update -d ./build/ModifiedModuleLoaderClient --no-dev
+
+# Rename config.php
+mv ./build/ModifiedModuleLoaderClient/config/_config.php ./build/ModifiedModuleLoaderClient/config/config.php
+
+# Delete directories
+rm -rf ./build/ModifiedModuleLoaderClient/docs
+rm -rf ./build/ModifiedModuleLoaderClient/tests
+rm -rf ./build/ModifiedModuleLoaderClient/patches
+
+# Delete files
+rm ./build/ModifiedModuleLoaderClient/README.md
+rm ./build/ModifiedModuleLoaderClient/.gitignore
+rm ./build/ModifiedModuleLoaderClient/composer.json
+rm ./build/ModifiedModuleLoaderClient/composer.lock
+rm ./build/ModifiedModuleLoaderClient/phpunit.xml
+rm ./build/ModifiedModuleLoaderClient/mmlc_installer.php
+rm ./build/ModifiedModuleLoaderClient/scripts/buildClient.sh
+
+# Create empty directories
+mkdir ./build/ModifiedModuleLoaderClient/Modules
+mkdir ./build/ModifiedModuleLoaderClient/Archives
+
+# Create tar file
+COPYFILE_DISABLE=1 tar -C ./build/ -cf ./build/ModifiedModuleLoaderClient.tar ModifiedModuleLoaderClient/
+
+rm -rf ./build/ModifiedModuleLoaderClient
