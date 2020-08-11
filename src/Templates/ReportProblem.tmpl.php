42d1e7bb85e0b89925f04ca530d53d9a030b9ecb
php-schubser <d.bjelajac@hoqmee.net>
2020-08-11 11:14:40 +0200

    feat: add send mail to developer

---
+++ src/Templates/ReportProblem.tmpl.php
@@ -0,0 +1,33 @@
+<?php if (!defined('LOADED_FROM_INDEX') || LOADED_FROM_INDEX != 'true') { die('Access denied.'); }?>
+
+<?php use RobinTheHood\ModifiedModuleLoaderClient\SendMail; ?>
+
+<?php 
+if (isset($_POST['send_mail'])) {
+    SendMail::sendFeedback();
+} ?>
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <?php include 'Head.tmpl.php' ?>
+    </head>
+    <body>
+        <?php include 'Navi.tmpl.php' ?>
+        <div class="content">
+        <?php echo RobinTheHood\ModifiedModuleLoaderClient\Notification::renderFlashMessages() ?>
+            <div class="container">
+              <div class="row">
+                <div class="offset-4 col-4">
+                  <form method="POST" action="#" >
+                    <input class="mb-2 w-100" placeholder="Ihr Name" type="text" required name="name" id="name">
+                    <input class="mb-2 w-100" placeholder="Ihre E-mail" type="email" required name="email" id="email">
+                    <textarea style="resize:none;" placeholder="Ihre Nachricht" rows="6" required class="mb-2 form-control" name="message"></textarea>
+                    <input name="send_mail" type="submit" class="button button-default" value="Nachricht absenden">
+                  </form>
+                </div>
+              </div>
+            </div>
+        </div>
+        <?php include 'Footer.tmpl.php' ?>
+    </body>
+</html>
