00e1bde3e165b319166b9b1ccd827c6fcb113026
Jay <j.trees@hybridsupply.de>
2020-08-31 15:48:42 +0200

    feat: login improvements (#35)
    - style: added logo with a hue animation
    - style: added a background image
    - style: added a heading
    - style: changed some colours on the login form

--- src/Templates/SignIn.tmpl.php
+++ src/Templates/SignIn.tmpl.php
@@ -6,19 +6,44 @@
         <?php include 'Head.tmpl.php' ?>
     </head>
 
-    <body>
+    <body id="signIn">
         <div class="content">
+            <div class="spacer top"></div>
+
+            <img src="src/Templates/Images/mmlc-logo-transparent.png" alt="MMLC Logo" class="logo">
+
             <div class="signin-frame">
                 <form action="?action=signIn" method="post">
-                    <label for="username">Username:</label>
-                    <input type="text" name="username" value="<?php echo empty($_POST['username']) ? '' :  $_POST['username'] ?>">
+                    <h1>MMLC Anmeldung</h1>
 
-                    <label for="password">Passwort:</label>
-                    <input type="password" name="password" value="">
+                    <fieldset>
+                        <label for="username">
+                            <picture>
+                                <source src="src/Templates/Images/user.png">
 
-                    <input type="submit" value=" Anmelden ">
+                                <img src="src/Templates/Images/user.png">
+                            </picture>
+                        </label>
+                        <input id="username" type="text" name="username" value="<?php echo empty($_POST['username']) ? '' :  $_POST['username'] ?>" placeholder="Benutzername">
+                    </fieldset>
+
+                    <fieldset>
+                        <label for="password">
+                            <picture>
+                                <source src="src/Templates/Images/password.png">
+
+                                <img src="src/Templates/Images/password.png">
+                            </picture>
+                        </label>
+
+                        <input id="password" type="password" name="password" value="" placeholder="Passwort">
+                    </fieldset>
+
+                    <input type="submit" value="Anmelden" class="primary">
                 </form>
             </div>
+
+            <div class="spacer bottom"></div>
         </div>
     </body>
 </html>
