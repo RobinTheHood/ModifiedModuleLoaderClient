fb2dbc1a18107e2cd3e31f97ddba6d19ebece251
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-03-06 18:46:10 +0100

    refactor: use <?= ... instead of <?php echo ... and outsource ModuleListungModule

--- src/Templates/ModuleListing.tmpl.php
+++ src/Templates/ModuleListing.tmpl.php
@@ -19,67 +19,27 @@ use RobinTheHood\ModifiedModuleLoaderClient\Notification;
         <div class="content module-listing">
 
             <div>
-                <?php echo RobinTheHood\ModifiedModuleLoaderClient\Notification::renderFlashMessages() ?>
+                <?= Notification::renderFlashMessages() ?>
 
                 <div class="search">
-                    <input type="text" value="" placeholder="Suche Modul- oder Archivname" onkeyup="filterModule(this.value)">
+                    <input type="text" value="" placeholder="Suche Modul- oder Archivname"
+                    onkeyup="filterModule(this.value)">
                 </div>
 
-                <?php if (isset($_GET['filterModules']) && $_GET['filterModules'] == 'loaded') { ?>
-                    <h1>Geladene Module</h1>
-                <?php } elseif (isset($_GET['filterModules']) && $_GET['filterModules'] == 'installed') { ?>
-                    <h1>Installierte Module</h1>
-                <?php } elseif (isset($_GET['filterModules']) && $_GET['filterModules'] == 'updatable') { ?>
-                    <h1>Aktualisierbare Module</h1>
-                <?php } elseif (isset($_GET['filterModules']) && $_GET['filterModules'] == 'changed') { ?>
-                    <h1>Geänderte Module</h1>
-                <?php } elseif (isset($_GET['filterModules']) && $_GET['filterModules'] == 'notloaded') { ?>
-                    <h1>Nicht geladene Module</h1>
-                <?php } else { ?>
-                    <h1>Alle Module</h1>
-                <?php } ?>
+                <h1><?= $heading ?></h1>
 
                 <div class="modules">
                     <?php foreach ($groupedModules as $category => $modules) { ?>
-                        <h2><?php echo RobinTheHood\ModifiedModuleLoaderClient\Category::getCategoryName($category); ?></h2>
-
+                        <h2><?= Category::getCategoryName($category); ?></h2>
                         <div class="category">
-                            <?php foreach ($modules as $module) { ?>
-                                <?php if ($module->getVisibility() == 'hidden') { continue; } ?>
-
-                                <div class="card module-serach-box <?php echo $module->isCompatible() ? 'compatible' : 'incompatible'; ?>" data-tags="<?php echo $module->getName(); ?> <?php echo $module->getArchiveName()?> <?php echo str_replace(',', ' ', $module->getTags())?>">
-                                    <a href="?action=moduleInfo&archiveName=<?php echo $module->getArchiveName()?>&version=<?php echo $module->getVersion() ?>">
-                                        <img src="<?php echo $module->getIconUri(); ?>" class="card-img-top" alt="<?php echo $module->getName(); ?>">
-                                    </a>
-
-                                    <div class="card-body">
-                                        <h5 class="card-title">
-                                            <?php echo $module->getName(); ?>
-                                            <span class="card-version"><?php echo $module->getVersion(); ?></span>
-                                        </h5>
-
-                                        <div class="module-price">
-                                            <?php if ($module->isInstalled()) {
-                                                echo 'installiert';
-                                            } else {
-                                                echo $module->getPriceFormated();
-                                            } ?>
-                                        </div>
-
-                                        <?php
-                                            $compatibility = $module->isCompatible() ? 'kompatibel' : 'inkompatibel';
-                                            $tooltip = $module->isCompatible() ? 'Dieses Modul wurde getestet und funktioniert mit deiner Version von modified.' : 'Dieses Modul wurde noch nicht mit deiner Version von modified getestet.';
-                                        ?>
-                                        <div class="card-compatibility" data-tooltip="<?php echo $tooltip; ?>">
-                                            <?php echo $compatibility; ?>
-                                        </div>
-
-                                        <p class="card-text"><?php echo strip_tags($module->getShortDescription()); ?></p>
-
-                                        <a href="?action=moduleInfo&archiveName=<?php echo $module->getArchiveName()?>&version=<?php echo $module->getVersion() ?>" class="btn <?php echo $module->isCompatible() ? 'btn-primary' : 'btn-secondary'; ?>">Details</a>
-                                    </div>
-                                </div>
-                            <?php } ?>
+                            <?php
+                            foreach ($modules as $module) {
+                                if ($module->getVisibility() == 'hidden') {
+                                    continue;
+                                }
+                                include 'ModuleListingModule.tmpl.php';
+                            }
+                            ?>
                         </div>
                     <?php } ?>
                 </div>
---
+++ src/Templates/ModuleListingModule.tmpl.php
@@ -0,0 +1,43 @@
+<?php
+
+defined('LOADED_FROM_INDEX') && LOADED_FROM_INDEX ?? die('Access denied.');
+
+$dataTags = $module->getName() . ' ' . str_replace(',', ' ', $module->getTags());
+                                
+if ($module->isCompatible()) {
+    $compatibility = 'kompatibel';
+    $tooltip = 'Dieses Modul wurde getestet und funktioniert mit deiner Version von modified.';
+} else {
+    $compatibility = 'inkompatibel';
+    $tooltip = 'Dieses Modul wurde noch nicht mit deiner Version von modified getestet.';
+}
+
+$modulePrice = $module->isInstalled() ? 'installiert' : $module->getPriceFormated();
+$moduleLink = '?action=moduleInfo&archiveName=' . $module->getArchiveName() . '&version=' . $module->getVersion();
+
+?>
+
+<div class="card module-serach-box <?= $module->isCompatible() ? 'compatible' : 'incompatible'; ?>" data-tags="<?= $dataTags ?>">
+    <a href="<?= $moduleLink ?>">
+        <img src="<?= $module->getIconUri(); ?>" class="card-img-top" alt="<?= $module->getName(); ?>">
+    </a>
+
+    <div class="card-body">
+        <h5 class="card-title">
+            <?= $module->getName(); ?>
+            <span class="card-version"><?= $module->getVersion(); ?></span>
+        </h5>
+
+        <div class="module-price">
+            <?= $modulePrice ?>
+        </div>
+
+        <div class="card-compatibility" data-tooltip="<?= $tooltip; ?>">
+            <?= $compatibility; ?>
+        </div>
+
+        <p class="card-text"><?= strip_tags($module->getShortDescription()); ?></p>
+
+        <a href="<?= $moduleLink ?>" class="btn <?= $module->isCompatible() ? 'btn-primary' : 'btn-secondary'; ?>">Details</a>
+    </div>
+</div>
