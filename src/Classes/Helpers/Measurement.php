38f65dde0e51f2edfa6868ab3e0bf93590fb729a
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-03-09 17:26:52 +0100

    feat: add new class Measurement

---
+++ src/Classes/Helpers/Measurement.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of MMLC - ModifiedModuleLoaderClient.
+ *
+ * (c) Robin Wieschendorf <mail@robinwieschendorf.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RobinTheHood\ModifiedModuleLoaderClient\Helpers;
+
+class Measurement
+{
+
+    public static function getTimeStamp(): string
+    {
+        $now = \DateTime::createFromFormat('U.u', number_format(microtime(true), 6, '.', ''));
+        return $now->format("H:i:s.u");
+    }
+}
