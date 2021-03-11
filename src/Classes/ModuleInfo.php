b12b8deb3878f7e2f1e85d2ac985f782184b54c8
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-03-11 19:41:15 +0100

    docs: improve PHPDoc

--- src/Classes/ModuleInfo.php
+++ src/Classes/ModuleInfo.php
@@ -13,78 +13,154 @@ namespace RobinTheHood\ModifiedModuleLoaderClient;
 
 use RobinTheHood\ModifiedModuleLoaderClient\Helpers\ArrayHelper;
 
+/**
+ * Die Klasse ModuleInfo repräsentiert die Daten einer moduleinfo.json
+ */
 class ModuleInfo
 {
     /**
+     * Name des Moduls in menschen lesbarer Form.
+     *
      * @var string
      */
     protected $name;
 
     /**
+     * Der eindeutige archiveName des Moduls. Der archiveName
+     * setzt sich aus den vendorName / moduleName zusammen.
+     *
+     * Beispiel: robinthehood/modified-std-module
+     *
      * @var string
      */
     protected $archiveName;
     
     /**
+     * Das Verzeichnis, in dem sich die Quellcode Dateien des Moduls befinden,
+     * die in den Shop kopiert/verlinkt werden sollen.
+     *
+     * Beispiel: new_fieles
+     *
      * @var string
      */
     protected $sourceDir;
     
     /**
+     * Die Version des Moduls. Die Version muss der Sermver konvention folgen.
+     * Der Wert darf auch 'auto' sein. In diesem Fall wird versucht, sich die
+     * Versionsnummer aus anderen Quellen zu holen wie z. B. aus git Tags.
+     *
+     * Beispiel: 1.1.0
+     *
      * @var string
      */
     protected $version;
     
     /**
+     * Eine Kurzbeschreibung des Moduls in menschen lesbarer Form.
+     *
      * @var string
      */
     protected $shortDescription;
     
     /**
+     * Eine Beschreibung des Moduls in menschen lesbarer Form.
+     *
      * @var string
      */
     protected $description;
     
     /**
+     * Name des Entwickler / Entwicklerfirma, die das Modul
+     * programmiert.
+     *
      * @var string
      */
     protected $developer;
     
     /**
+     * HIER FEHLT EINE BESCHREIBUNG
+     *
      * @var string
      */
     protected $developerWebsite;
     
     /**
+     * HIER FEHLT EINE BESCHREIBUNG
+     *
      * @var string
      */
     protected $website;
     
-
+    /**
+     * Ein Array mit archiveNamen mit Version, von denen das Modul
+     * abhängig ist.
+     *
+     * Beispiel:
+     *
+     * @var array
+     */
     protected $require;
     
     /**
+     * Die Kategorie in der sich das Modul befindet.
+     *
      * @var string
      */
     protected $category;
     
     /**
+     * HIER FEHLT EINE BESCHREIBUNG
+     *
      * @var string
      */
     protected $type;
     
-
+    /**
+     * Mit welchen Version von Modified ist das Modul kompatible.
+     *
+     * Beispiel: [
+     *  2.0.4.2,
+     *  2.0.5.1
+     * ]
+     *
+     * @var array
+     */
     protected $modifiedCompatibility;
     
     /**
+     * Installtionsanleitung in menschen lesbarer Form.
+     *
      * @var string
      */
     protected $installation;
+
+    /**
+     * HIER FEHLT EINE BESCHREIBUNG
+     *
+     * @var string
+     */
     protected $visibility;
+
+    /**
+     * HIER FEHLT EINE BESCHREIBUNG
+     *
+     * @var string
+     */
     protected $price;
+
+    /**
+     * HIER FEHLT EINE BESCHREIBUNG
+     *
+     * @var array
+     */
     protected $autoload;
 
     /**
+     * Tags mit Komma getrennt.
+     *
+     * Beispiel: modul, modified, seo
+     *
      * @var string
      */
     protected $tags;
