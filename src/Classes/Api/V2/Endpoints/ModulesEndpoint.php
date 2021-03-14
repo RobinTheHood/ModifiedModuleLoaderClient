b8d897a60d8d1c53f8311b386ac4f24b464b0da5
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-03-14 18:05:34 +0100

    feat: use apiToken

--- src/Classes/Api/V2/Endpoints/ModulesEndpoint.php
+++ src/Classes/Api/V2/Endpoints/ModulesEndpoint.php
@@ -12,6 +12,8 @@
 namespace RobinTheHood\ModifiedModuleLoaderClient\Api\V2\Endpoints;
 
 use Buzz\Browser;
+use RobinTheHood\ModifiedModuleLoaderClient\Api\V2\ApiException;
+use RobinTheHood\ModifiedModuleLoaderClient\Notification;
 
 class ModulesEndpoint extends AbstractEndpoint
 {
@@ -22,8 +24,25 @@ class ModulesEndpoint extends AbstractEndpoint
     public function getAllBy(array $parameters)
     {
         $this->convertBoolToString($parameters);
+
+        $header = [];
+        if ($this->apiToken) {
+            $header['Authorization'] = 'Bearer ' . $this->apiToken;
+        }
+
         $url = $this->resourcePath . '?' . http_build_query($parameters);
-        $response = $this->browser->get($url);
+        $response = $this->browser->get($url, $header);
+
+        if ($response->getStatusCode() >= 400) {
+            //throw new ApiException();
+
+            // TODO: DO NOT USE Notification, throw ApiException
+            Notification::pushFlashMessage([
+                'text' => 'Error: Bad response. {MESSAGE}',
+                'type' => 'error'
+            ]);
+        }
+
         return $response->getBody()->getContents();
     }
 }
