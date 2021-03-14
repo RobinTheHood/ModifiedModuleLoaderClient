22184b464493ea85c50fba7ab411f7245b975c0f
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-03-14 18:52:23 +0100

    feat: add ApiException

---
+++ src/Classes/Api/V2/ApiException.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of MMLC - ModifiedModuleLoaderClient.
+ *
+ * (c) Robin Wieschendorf <mail@robinwieschendorf.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RobinTheHood\ModifiedModuleLoaderClient\Api\V2;
+
+class ApiException extends \Exception
+{
+}
