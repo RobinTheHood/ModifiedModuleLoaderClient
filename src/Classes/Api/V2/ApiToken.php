a5b7811839961063db171ba9a74b91bb4767a8a3
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-03-17 12:04:50 +0100

    feat: use new class ApiToken

---
+++ src/Classes/Api/V2/ApiToken.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of MMLC - ModifiedModuleLoaderClient.
+ *
+ * (c) Robin Wieschendorf <mail@robinwieschendorf.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RobinTheHood\ModifiedModuleLoaderClient\Api\V2;
+
+class ApiToken
+{
+    /** @var string */
+    private $token;
+
+    public function __construct(string $token)
+    {
+        $this->token = $token;
+    }
+
+    public function getToken(): string
+    {
+        return $this->token;
+    }
+
+    public function createBearer()
+    {
+        return 'Bearer ' . $this->token;
+    }
+}
