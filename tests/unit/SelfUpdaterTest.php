6a13b4087fdbd47f63e7962e1c3a1f72eb45d94e
Robin Wieschendorf <mail@robinwieschendorf.de>
2022-04-05 14:19:47 +0200

    test: add SelfUpdaterTest

---
+++ tests/unit/SelfUpdaterTest.php
@@ -0,0 +1,115 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of MMLC - ModifiedModuleLoaderClient.
+ *
+ * (c) Robin Wieschendorf <mail@robinwieschendorf.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RobinTheHood\ModifiedModuleLoaderClient\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use RobinTheHood\ModifiedModuleLoaderClient\Api\V1\ApiRequest;
+use RobinTheHood\ModifiedModuleLoaderClient\SelfUpdater;
+
+class SelfUpdaterTest extends TestCase
+{
+    public function getStubedApiRequest()
+    {
+        /** @var MockObject $stubApiRequest */
+        $stubApiRequest = $this->createMock(ApiRequest::class);
+        $stubApiRequest->method('getAllVersions')->willReturn([
+            'content' => [
+                [
+                    'version' => '1.1.1-alpha',
+                    'fileName' => 'ModifiedModuleLoaderClient_v1.1.1-alpha.tar'
+                ],
+                [
+                    'version' => '1.1.2-alpha',
+                    'fileName' => 'ModifiedModuleLoaderClient_v1.1.2-alpha.tar'
+                ],
+                [
+                    'version' => '1.1.2',
+                    'fileName' => 'ModifiedModuleLoaderClient_v1.1.2.tar'
+                ],
+                [
+                    'version' => '1.1.3-alpha',
+                    'fileName' => 'ModifiedModuleLoaderClient_v1.1.3-alpha.tar'
+                ],
+            ]
+        ]);
+
+        return $stubApiRequest;
+    }
+
+    public function testGetInstalledVersion()
+    {
+        $selfUpdater = new SelfUpdater();
+        $version = $selfUpdater->getInstalledVersion();
+
+        $this->assertNotEmpty($version);
+        $this->assertIsString($version);
+    }
+
+    public function testGetNewestVersionInfo()
+    {
+        // Arrage
+        $stubApiRequest = $this->getStubedApiRequest();
+        $selfUpdater = new SelfUpdater($stubApiRequest);
+
+        // Act
+        $newestLatestVersionInfo = $selfUpdater->getNewestVersionInfo(true);
+        $newestStabelVersionInfo = $selfUpdater->getNewestVersionInfo(false);
+
+        // Assert
+        $expectsLatest = [
+            'version' => '1.1.3-alpha',
+            'fileName' => 'ModifiedModuleLoaderClient_v1.1.3-alpha.tar'
+        ];
+
+        $expectsStable = [
+            'version' => '1.1.2',
+            'fileName' => 'ModifiedModuleLoaderClient_v1.1.2.tar'
+        ];
+
+        $this->assertEquals($expectsLatest, $newestLatestVersionInfo);
+        $this->assertEquals($expectsStable, $newestStabelVersionInfo);
+    }
+
+    public function testGetVersionInfos()
+    {
+        // Arrage
+        $stubApiRequest = $this->getStubedApiRequest();
+        $selfUpdater = new SelfUpdater($stubApiRequest);
+
+        // Act
+        $versionInfos = $selfUpdater->getVersionInfos(true);
+
+        // Assert
+        $expects = [
+            [
+                'version' => '1.1.1-alpha',
+                'fileName' => 'ModifiedModuleLoaderClient_v1.1.1-alpha.tar'
+            ],
+            [
+                'version' => '1.1.2-alpha',
+                'fileName' => 'ModifiedModuleLoaderClient_v1.1.2-alpha.tar'
+            ],
+            [
+                'version' => '1.1.2',
+                'fileName' => 'ModifiedModuleLoaderClient_v1.1.2.tar'
+            ],
+            [
+                'version' => '1.1.3-alpha',
+                'fileName' => 'ModifiedModuleLoaderClient_v1.1.3-alpha.tar'
+            ],
+        ];
+
+        $this->assertEquals($expects, $versionInfos);
+    }
+}
