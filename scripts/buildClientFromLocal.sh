4baa662c20ac52d461e10710865e719225435244
Robin Wieschendorf <mail@robinwieschendorf.de>
2022-05-06 13:26:48 +0200

    build: improve buildClientFromLocal.sh script

--- scripts/buildClientFromLocal.sh
+++ scripts/buildClientFromLocal.sh
@@ -20,7 +20,6 @@ cp -r ./config ./build/ModifiedModuleLoaderClientLocal/config
 cp -r ./docs ./build/ModifiedModuleLoaderClientLocal/docs
 cp -r ./scripts ./build/ModifiedModuleLoaderClientLocal/scripts
 cp -r ./src ./build/ModifiedModuleLoaderClientLocal/src
-cp -r ./tests ./build/ModifiedModuleLoaderClientLocal/tests
 cp -r ./vendor ./build/ModifiedModuleLoaderClientLocal/vendor
 cp ./index.php ./build/ModifiedModuleLoaderClientLocal/index.php
 
@@ -31,22 +30,27 @@ rm ./build/ModifiedModuleLoaderClientLocal/config/version.json
 mv ./build/ModifiedModuleLoaderClientLocal/config/_config.php ./build/ModifiedModuleLoaderClientLocal/config/config.php
 echo "{\"version\": \"${NEW_VERSION}\"}" > ./build/ModifiedModuleLoaderClientLocal/config/version.json
 
-# Delete directories
+# Delete directories even if they do not exist
+rm -rf ./build/ModifiedModuleLoaderClientLocal/private
 rm -rf ./build/ModifiedModuleLoaderClientLocal/docs
 rm -rf ./build/ModifiedModuleLoaderClientLocal/tests
 rm -rf ./build/ModifiedModuleLoaderClientLocal/patches
+rm -rf ./build/ModifiedModuleLoaderClientLocal/Modules
+rm -rf ./build/ModifiedModuleLoaderClientLocal/Archives
 rm -rf ./build/ModifiedModuleLoaderClientLocal/.github
 
 # Delete files
 rm ./build/ModifiedModuleLoaderClientLocal/README.md
-rm ./build/ModifiedModuleLoaderClientLocal/.gitignore
 rm ./build/ModifiedModuleLoaderClientLocal/composer.json
 rm ./build/ModifiedModuleLoaderClientLocal/composer.lock
 rm ./build/ModifiedModuleLoaderClientLocal/phpunit.xml
 rm ./build/ModifiedModuleLoaderClientLocal/psalm.xml
 rm ./build/ModifiedModuleLoaderClientLocal/psalm-baseline.xml
 rm ./build/ModifiedModuleLoaderClientLocal/mmlc_installer.php
+rm ./build/ModifiedModuleLoaderClientLocal/icon.png
 rm ./build/ModifiedModuleLoaderClientLocal/scripts/buildClient.sh
+rm ./build/ModifiedModuleLoaderClientLocal/scripts/buildClientFromLocal.sh
+rm ./build/ModifiedModuleLoaderClientLocal/.gitignore
 
 # Create empty directories
 mkdir ./build/ModifiedModuleLoaderClientLocal/Modules
