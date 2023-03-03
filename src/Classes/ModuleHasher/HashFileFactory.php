f232fb7e04e6ed4472e59d93dede3dab4afad21c
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-03-03 17:14:36 +0100

    chore: add class HashFileFactory

---
+++ src/Classes/ModuleHasher/HashFileFactory.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of MMLC - ModifiedModuleLoaderClient.
+ *
+ * (c) Robin Wieschendorf <mail@robinwieschendorf.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+declare(strict_types=1);
+
+namespace RobinTheHood\ModifiedModuleLoaderClient\ModuleHasher;
+
+class HashFileFactory
+{
+    public static function createFromArray(array $array): HashFile
+    {
+        $hashFile = new HashFile();
+        $hashFile->array = $array;
+        return $hashFile;
+    }
+
+    public static function createFromJson(string $json): HashFile
+    {
+        $array = json_decode($json, true);
+
+        $version = self::getVersion($array);
+        if ($version === '0.1.0') {
+            $array = self::convertToVersion020($array);
+        }
+
+        return self::createFromArray($array);
+    }
+
+    private static function getVersion(array $array): string
+    {
+        if (array_key_exists('version', $array)) {
+            return $array['version'];
+        }
+
+        if (array_key_exists('scopes', $array)) {
+            if (is_array($array['scopes'])) {
+                return '0.2.0';
+            }
+        }
+
+        // Version 0.1.0 is the modulehash.json Version of MMLC <= 1.20.0
+        // This modulehash.json has no version information an is internal
+        // handelt as version 0.1.0.
+        return '0.1.0';
+    }
+
+    private static function convertToVersion020(array $array): array
+    {
+        $newArray = [
+            'scopes' => [
+                'src' => [
+                    'hashes' => $array
+                ]
+            ]
+        ];
+        return $newArray;
+    }
+}
---
+++ tests/unit/ModuleHasherTests/HashFileFactoryTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of MMLC - ModifiedModuleLoaderClient.
+ *
+ * (c) Robin Wieschendorf <mail@robinwieschendorf.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace RobinTheHood\ModifiedModuleLoaderClient\Tests\Unit\ModuleHahserTests;
+
+use PHPUnit\Framework\TestCase;
+
+class HashFileFactoryTest extends TestCase
+{
+    public function testCreateFromArray()
+    {
+    }
+
+    public function testCreateFromJson()
+    {
+    }
+}
