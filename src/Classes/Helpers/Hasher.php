a65e5f135212f506ee28407fa0b762eb76bdabd5
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-03-03 21:45:05 +0100

    chore: remove unused code

--- src/Classes/Helpers/Hasher.php
+++ src/Classes/Helpers/Hasher.php
@@ -34,19 +34,6 @@ class Hasher
 
         $jsonStr = file_get_contents($path);
         $hashes = json_decode($jsonStr, true);
-
-        if (!array_key_exists('scopes', $hashes)) {
-            $hashes = [
-                'scopes' => [
-                    'src' => [
-                        'hashes' => $hashes
-                    ]
-                ]
-            ];
-        }
-
-        // var_dump($hashes);
-        // die();
         return $hashes;
     }
 
@@ -94,92 +81,5 @@ class Hasher
         }
 
         return $changedFiles;
-    }
-
-    /**
-     *
-     * HashEntries = [
-     *     [file => hash],
-     *     [file => hash],
-     *     ...
-     * ]
-     *
-     * ChangeEntries = [
-     *     [file => type],
-     *     [file => type],
-     *     ...
-     * ]
-     *
-     * @param array $installed HashEntires
-     * @param array $shop HashEntires
-     * @param array $mmlc HashEntires
-     *
-     * @return array ChangeEntries
-     */
-    public function getChangedEntries(array $installed, array $shop, array $mmlc): array
-    {
-        $new = $this->getAllHashesANotInB($mmlc, $installed);
-        $deleted = $this->getAllHashesANotInB($installed, $shop);
-        $changed1 = $this->getHashesEntriesANotEqualToB($installed, $shop);
-        $changed2 = $this->getHashesEntriesANotEqualToB($mmlc, $installed);
-
-        $changeEntries = [];
-        $changeEntries = $this->addChanges($changeEntries, $new, 'new');
-        $changeEntries = $this->addChanges($changeEntries, $deleted, 'deleted');
-        $changeEntries = $this->addChanges($changeEntries, $changed1, 'changed');
-        $changeEntries = $this->addChanges($changeEntries, $changed2, 'changed');
-
-        return $changeEntries;
-    }
-
-    private function addChanges(array $changeEntries, array $hashEntires, string $type)
-    {
-        foreach ($hashEntires as $file => $hash) {
-            $changeEntries[$file] = $type;
-        }
-        return $changeEntries;
-    }
-
-    /**
-     * Gibt alle HashEntries aus A zurück, deren File nicht in B enthalten sind.
-     * HashEntries = [
-     *     [file => hash],
-     *     [file => hash],
-     *     ...
-     * ]
-     */
-    private function getAllHashesANotInB(array $hashesEntriesA, array $hashesEntriesB): array
-    {
-        $hashEntries = [];
-        foreach ($hashesEntriesA as $fileA => $hashA) {
-            if (!isset($hashesEntriesB[$fileA])) {
-                $hashes[] = $hashesEntriesA;
-            }
-        }
-        return $hashEntries;
-    }
-
-    /**
-     * Gibt alle HashEntries aus A zurück, deren File in B enthalten ist und die
-     * Hashes von A und B unterschiedlich sind.
-     * HashEntries = [
-     *     [file => hash],
-     *     [file => hash],
-     *     ...
-     * ]
-     */
-    private function getHashesEntriesANotEqualToB(array $hashesEntriesA, array $hashesEntriesB): array
-    {
-        $hashEntries = [];
-        foreach ($hashesEntriesA as $fileA => $hashA) {
-            if (!isset($hashesEntriesB[$fileA])) {
-                continue;
-            }
-
-            if ($hashA !== $hashesEntriesB[$fileA]) {
-                $hashEntries[] = $hashA;
-            }
-        }
-        return $hashEntries;
     }
 }
