06ee84f7a28f8efe97145679a3ee8b12dd9eea43
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-03-03 17:14:00 +0100

    chore: add class FileHasher and FileHasherInterface

---
+++ src/Classes/ModuleHasher/FileHasherInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of MMLC - ModifiedModuleLoaderClient.
+ *
+ * (c) Robin Wieschendorf <mail@robinwieschendorf.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace RobinTheHood\ModifiedModuleLoaderClient\ModuleHasher;
+
+interface FileHasherInterface
+{
+    public function createHashes(array $paths, string $basePath = ''): HashEntryCollection;
+}
