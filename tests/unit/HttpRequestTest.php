495ff92a0ec1ab1afd7ea972000c75c426a4fe5a
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-03-18 20:16:27 +0100

    test: add test for HttpRequest

---
+++ tests/unit/HttpRequestTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of MMLC - ModifiedModuleLoaderClient.
+ *
+ * (c) Robin Wieschendorf <mail@robinwieschendorf.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RobinTheHood\ModifiedModuleLoaderClient\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use RobinTheHood\ModifiedModuleLoaderClient\Api\V1\HttpRequest;
+
+class HttpRequestTest extends TestCase
+{
+    public function testSendPostRequestSuccess()
+    {
+        $httpClient = new HttpRequest();
+        $response = $httpClient->sendPostRequest('https://postman-echo.com/post', ['foo' => 'bar']);
+
+        $this->assertNotNull($response);
+        $this->assertIsString($response);
+        $this->assertStringContainsString('application/x-www-form-urlencoded', $response);
+        $this->assertStringContainsString('"foo": "bar"', $response);
+    }
+
+    public function testSendPostRequestFailure()
+    {
+        $httpClient = new HttpRequest();
+        $response = $httpClient->sendPostRequest('https://this-url-does-not-exist.com', []);
+        $this->assertIsString($response);
+    }
+}
