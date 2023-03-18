7da0e47a068dfe07550cdbeb7374d6f2a7921ca2
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-03-18 20:15:41 +0100

    refactor: use curl instead of file_get_contents

--- src/Classes/Api/V1/HttpRequest.php
+++ src/Classes/Api/V1/HttpRequest.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace RobinTheHood\ModifiedModuleLoaderClient\Api\V1;
 
+use Exception;
 use RobinTheHood\ModifiedModuleLoaderClient\App;
 
 class HttpRequest
@@ -28,60 +29,57 @@ class HttpRequest
         return false;
     }
 
-    public function sendPostRequest(string $url, $data)
+    public function sendPostRequest(string $url, $data): string
     {
-        // http verwenden, auch wenn die Url mit https://... beginnt
-        $options = [
-            'http' => [
-                'user_agent' => 'Modified Module Loader Client',
-                'method' => 'POST',
-                'header' => implode("\r\n", [
-                    'Content-type: application/x-www-form-urlencoded;'
-                ]),
-                'content' => http_build_query($data)
-            ]
-        ];
-        $context  = stream_context_create($options);
-        $result = @file_get_contents($url, false, $context);
+        try {
+            // HTTP POST-Request konfigurieren und senden
+            $result = $this->sendCurlPostRequest($url, $data);
 
-        // Logging
-        if ($this->logging) {
-            $logFilepath = App::getLogsRoot() . '/log.txt';
-            $logDirectory = dirname($logFilepath);
+            // Logging
+            if ($this->logging) {
+                $logFilepath = App::getLogsRoot() . '/log.txt';
+                $logDirectory = dirname($logFilepath);
 
-            if (!file_exists($logDirectory)) {
-                mkdir($logDirectory);
+                if (!file_exists($logDirectory)) {
+                    mkdir($logDirectory);
+                }
+
+                file_put_contents($logFilepath, $result);
             }
 
-            file_put_contents($logFilepath, $result);
+            return $result;
+        } catch (Exception $e) {
+            // Fehler behandeln
+            //error_log($e->getMessage());
+            return '';
         }
-
-        return $result;
     }
 
 
-    public function sendGetRequest($url)
+    public function sendGetRequest(string $url): string
     {
-        // http verwenden, auch wenn die Url mit https://... beginnt
-        $options = [
-            'http' => [
-                'user_agent' => 'Modified Module Loader Client',
-                'method' => "GET",
-                'header' => implode("\r\n", [
-                    'Content-type: text/plain;'
-                ])
-            ]
-        ];
+        try {
+            // HTTP GET-Request konfigurieren und senden
+            $result = $this->sendCurlGetRequest($url);
 
-        $context = stream_context_create($options);
-        $result = @file_get_contents($url, false, $context);
+            // Logging
+            if ($this->logging) {
+                $logFilepath = App::getLogsRoot() . '/log.txt';
+                $logDirectory = dirname($logFilepath);
 
-        // Logging
-        if ($this->logging) {
-            file_put_contents(App::getLogsRoot() . '/log.txt', $result);
+                if (!file_exists($logDirectory)) {
+                    mkdir($logDirectory);
+                }
+
+                file_put_contents($logFilepath, $result);
+            }
+
+            return $result;
+        } catch (Exception $e) {
+            // Fehler behandeln
+            //error_log($e->getMessage());
+            return '';
         }
-
-        return $result;
     }
 
     public static function createQuery(array $queryValues): string
@@ -91,5 +89,61 @@ class HttpRequest
             $query .= $name . '=' . urlencode($value) . '&';
         }
         return $query;
+    }
+
+    private function sendCurlGetRequest(string $url)
+    {
+        // HTTP GET-Request konfigurieren
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_USERAGENT, 'Modified Module Loader Client');
+
+        // Request senden und Ergebnis erhalten
+        $result = curl_exec($curl);
+
+        // HTTP-Statuscode und Fehler prüfen
+        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        if ($result === false) {
+            $error = curl_error($curl);
+            curl_close($curl);
+            throw new Exception('Fehler beim Senden des GET-Requests: ' . $error);
+        } elseif ($httpCode < 200 || $httpCode >= 300) {
+            curl_close($curl);
+            throw new Exception('Fehler beim Senden des GET-Requests: HTTP-Statuscode ' . $httpCode);
+        }
+
+        // Request beenden
+        curl_close($curl);
+
+        return $result;
+    }
+
+    private function sendCurlPostRequest(string $url, $data)
+    {
+        // HTTP POST-Request konfigurieren
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_USERAGENT, 'Modified Module Loader Client');
+
+        // Request senden und Ergebnis erhalten
+        $result = curl_exec($curl);
+
+        // HTTP-Statuscode und Fehler prüfen
+        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        if ($result === false) {
+            $error = curl_error($curl);
+            curl_close($curl);
+            throw new Exception('Fehler beim Senden des POST-Requests: ' . $error);
+        } elseif ($httpCode < 200 || $httpCode >= 300) {
+            curl_close($curl);
+            throw new Exception('Fehler beim Senden des POST-Requests: HTTP-Statuscode ' . $httpCode);
+        }
+
+        // Request beenden
+        curl_close($curl);
+
+        return $result;
     }
 }
