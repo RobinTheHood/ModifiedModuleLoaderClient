8d97dcfabb178a2b10c29a03ba9323d5af7842d5
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-03-26 20:47:08 +0200

    feat: add new methods and tests SystemSet::getArchiveNames() and SystemSet::getArchives()

--- src/Classes/DependencyManager/SystemSet.php
+++ src/Classes/DependencyManager/SystemSet.php
@@ -15,5 +15,32 @@ namespace RobinTheHood\ModifiedModuleLoaderClient\DependencyManager;
 
 class SystemSet
 {
+    /** @var array<string,string> */
     public $systems = [];
+
+    /**
+     * @return string[]
+     */
+    public function getArchiveNames(): array
+    {
+        $archiveNames = array_values(array_filter(array_keys($this->systems), function ($name) {
+            return strpos($name, '/') !== false;
+        }));
+        return $archiveNames;
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function getArchives(): array
+    {
+        $archives = [];
+        foreach ($this->systems as $name => $version) {
+            if (!strpos($name, '/')) {
+                continue;
+            }
+            $archives[$name] = $version;
+        }
+        return $archives;
+    }
 }
---
+++ tests/unit/DependencyManager/SystemSetTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of MMLC - ModifiedModuleLoaderClient.
+ *
+ * (c) Robin Wieschendorf <mail@robinwieschendorf.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RobinTheHood\ModifiedModuleLoaderClient\Tests\Unit\DependencyManager;
+
+use PHPUnit\Framework\TestCase;
+use RobinTheHood\ModifiedModuleLoaderClient\DependencyManager\Combination;
+use RobinTheHood\ModifiedModuleLoaderClient\DependencyManager\SystemSet;
+
+class SystemSetTest extends TestCase
+{
+    public function testGetArchiveNamesReturnsExpectedArray()
+    {
+        // Arrange
+        $systems = [
+            'systemA' => '1.0',
+            'systemB/1.2' => '1.2',
+            'systemC/2.0' => '2.0',
+            'systemD/2.1' => '2.1',
+        ];
+        $systemSet = new SystemSet();
+        $systemSet->systems = $systems;
+
+        // Act
+        $result = $systemSet->getArchiveNames();
+
+        // Assert
+        $expected = ['systemB/1.2', 'systemC/2.0', 'systemD/2.1'];
+        $this->assertEquals($expected, $result);
+    }
+}
