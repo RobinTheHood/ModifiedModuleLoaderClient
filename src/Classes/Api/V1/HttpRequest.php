84835bfa1e237df3ec05457c6815b451ce436a48
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-03-28 21:03:25 +0200

    refactor: remove unused code

--- src/Classes/Api/V1/HttpRequest.php
+++ src/Classes/Api/V1/HttpRequest.php
@@ -13,7 +13,6 @@ declare(strict_types=1);
 
 namespace RobinTheHood\ModifiedModuleLoaderClient\Api\V1;
 
-use RobinTheHood\ModifiedModuleLoaderClient\Logger;
 use RobinTheHood\ModifiedModuleLoaderClient\Logger\LogLevel;
 use RobinTheHood\ModifiedModuleLoaderClient\Logger\StaticLogger;
 
