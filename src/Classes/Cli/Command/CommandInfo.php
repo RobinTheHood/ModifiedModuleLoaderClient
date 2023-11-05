4644557e125a27e4701cc9f15263647f2664268b
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-11-05 12:11:20 +0100

    fix: add missing commands

---
+++ src/Classes/Cli/Command/CommandInfo.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of MMLC - ModifiedModuleLoaderClient.
+ *
+ * (c) Robin Wieschendorf <mail@robinwieschendorf.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RobinTheHood\ModifiedModuleLoaderClient\Cli\Command;
+
+use RobinTheHood\ModifiedModuleLoaderClient\Loader\RemoteModuleLoader;
+
+class CommandInfo
+{
+    public function __construct()
+    {
+    }
+
+    public function run($archiveName): void
+    {
+        if (!$archiveName) {
+            echo "No archiveName specified. \n";
+            return;
+        }
+
+        $remoteModuleLoader = RemoteModuleLoader::create();
+        $module = $remoteModuleLoader->loadLatestVersionByArchiveName($archiveName);
+
+        if (!$module) {
+            echo "Module $archiveName not found. \n";
+            return;
+        }
+
+        echo "archiveName:       " . $module->getArchiveName() . "\n";
+        echo "latestVersion:     " . $module->getVersion() . "\n";
+        echo "moduleName:        " . $module->getName() . "\n";
+        echo "shortDescription:  " . $module->getShortDescription() . "\n";
+    }
+
+    public function help()
+    {
+        echo "\e[33mDescription:\e[0m\n";
+        echo "  Display information and details for a specific module.\n";
+        echo "\n";
+        echo "\e[33mUsage:\e[0m\n";
+        echo "  info <archiveName>\n";
+    }
+}
---
+++ src/Classes/Cli/Command/CommandWatch.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of MMLC - ModifiedModuleLoaderClient.
+ *
+ * (c) Robin Wieschendorf <mail@robinwieschendorf.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RobinTheHood\ModifiedModuleLoaderClient\Cli\Command;
+
+use RobinTheHood\ModifiedModuleLoaderClient\App;
+use RobinTheHood\ModifiedModuleLoaderClient\Cli\DirectoryWatcher;
+use RobinTheHood\ModifiedModuleLoaderClient\Helpers\FileHelper;
+
+class CommandWatch
+{
+    public function __construct()
+    {
+    }
+
+    public function run()
+    {
+        $basePath = App::getModulesRoot();
+        $directory = App::getModulesRoot();
+
+        echo "Watching the directory \033[32mModules\033[0m ...\n";
+
+        $dircectoryWatcher = new DirectoryWatcher();
+        $dircectoryWatcher->init($directory);
+        $dircectoryWatcher->watch(function ($changes) use ($basePath) {
+            if (!$changes) {
+                return;
+            }
+
+            foreach ($changes as $filePath => $status) {
+                $relativeFilePath = FileHelper::stripBasePath($basePath, $filePath);
+
+                if ($status === DirectoryWatcher::STATUS_NEW) {
+                    echo "\033[32mFile added:\033[0m $relativeFilePath\n";
+                } elseif ($status === DirectoryWatcher::STATUS_CHANGED) {
+                    echo "\033[33mFile modified:\033[0m $relativeFilePath\n";
+                } elseif ($status === DirectoryWatcher::STATUS_DELETED) {
+                    echo "\033[31mFile deleted:\033[0m $relativeFilePath\n";
+                }
+            }
+        });
+    }
+}
