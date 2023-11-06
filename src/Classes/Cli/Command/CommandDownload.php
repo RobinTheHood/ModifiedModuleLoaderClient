9f5fc4b9719df3dc7b698530b22747b6080560fb
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-11-06 14:49:00 +0100

    feat: start implementing command `download`

--- src/Classes/Cli/Command/CommandDownload.php
+++ src/Classes/Cli/Command/CommandDownload.php
@@ -15,6 +15,8 @@ namespace RobinTheHood\ModifiedModuleLoaderClient\Cli\Command;
 
 use RobinTheHood\ModifiedModuleLoaderClient\Cli\MmlcCli;
 use RobinTheHood\ModifiedModuleLoaderClient\Cli\TextRenderer;
+use RobinTheHood\ModifiedModuleLoaderClient\Loader\ModuleLoader;
+use RobinTheHood\ModifiedModuleLoaderClient\ModuleInstaller;
 
 class CommandDownload implements CommandInterface
 {
@@ -29,15 +31,60 @@ class CommandDownload implements CommandInterface
 
     public function run(MmlcCli $cli): void
     {
+        $archiveName = $cli->getFilteredArgument(0);
+
+        if (!$archiveName) {
+            $this->runHelp($cli);
+            return;
+        }
+
+        $moduleLoader = ModuleLoader::createFromConfig();
+        $module = $moduleLoader->loadLatestVersionByArchiveName($archiveName);
+
+        if (!$module) {
+            echo "Module " . TextRenderer::color($archiveName, TextRenderer::COLOR_GREEN) . " not found.\n";
+            return;
+        }
+
+        if ($module->isLoaded()) {
+            echo "Module " . TextRenderer::color($archiveName, TextRenderer::COLOR_GREEN)
+                . " version " . TextRenderer::color($module->getVersion(), TextRenderer::COLOR_YELLOW)
+                . " is already downloaded.\n";
+            return;
+        }
+
+        echo "Download module " . TextRenderer::color($archiveName, TextRenderer::COLOR_GREEN)
+            . " version " . TextRenderer::color($module->getVersion(), TextRenderer::COLOR_YELLOW)
+            . " ...\n";
+
+        $moduleInstaller = ModuleInstaller::createFromConfig();
+
+        if (!$moduleInstaller->pull($module)) {
+            echo TextRenderer::color('Error:', TextRenderer::COLOR_RED)
+                . " the module $archiveName could not be loaded. \n";
+        }
+
+        echo TextRenderer::color('ready', TextRenderer::COLOR_GREEN) . "\n";
     }
 
     public function runHelp(MmlcCli $cli): void
     {
         TextRenderer::renderHelpHeading('Description:');
-        echo "  Lorem\n";
+        echo "  Downloads a available MMLC Module from the Internet.\n";
         echo "\n";
 
         TextRenderer::renderHelpHeading('Usage:');
-        echo "  download ...\n";
+        echo "  download <archiveName>\n";
+        echo "\n";
+
+        TextRenderer::renderHelpHeading('Arguments:');
+        TextRenderer::renderHelpArgument('archiveName', 'The archiveName of the module to be loaded.');
+        echo "\n";
+
+        TextRenderer::renderHelpHeading('Options:');
+        TextRenderer::renderHelpOption('h', 'help', 'Display help for the given command.');
+        echo "\n";
+
+        echo "Read more at https://module-loader.de/documentation.php\n";
     }
 }
