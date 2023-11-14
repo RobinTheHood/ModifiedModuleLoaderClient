6f44386a460f580805845cf3dfb4b28202791055
grandeljay <github@grandels.email>
2023-11-14 14:16:57 +0100

    refactor: remove unused method `renderLogo`

--- src/Classes/Cli/TextRenderer.php
+++ src/Classes/Cli/TextRenderer.php
@@ -29,23 +29,6 @@ class TextRenderer
         return \str_pad($text, $length, ' ', \STR_PAD_RIGHT);
     }
 
-    public static function renderLogo()
-    {
-        // echo "    __  _____  _____    ______   ________    ____\n";
-        // echo "   /  |/  /  |/  / /   / ____/  / ____/ /   /  _/\n";
-        // echo "  / /|_/ / /|_/ / /   / /      / /   / /    / /  \n";
-        // echo " / /  / / /  / / /___/ /___   / /___/ /____/ /   \n";
-        // echo "/_/  /_/_/  /_/_____/\____/   \____/_____/___/   \n";
-        // created with: https://patorjk.com/software/taag/#p=display&f=Slant&t=MMLC%20CLI
-
-        echo "    __  ___ __  ___ __    ______   ______ __     ____\n";
-        echo "   /  |/  //  |/  // /   / ____/  / ____// /    /  _/\n";
-        echo "  / /|_/ // /|_/ // /   / /      / /    / /     / /  \n";
-        echo " / /  / // /  / // /___/ /___   / /___ / /___ _/ /   \n";
-        echo "/_/  /_//_/  /_//_____/\____/   \____//_____//___/   \n";
-        // cretated with: https://patorjk.com/software/taag/#p=display&h=1&f=Slant&t=MMLC%20CLI
-    }
-
     public static function getMaxLength(array $items): int
     {
         $maxLength = 0;
