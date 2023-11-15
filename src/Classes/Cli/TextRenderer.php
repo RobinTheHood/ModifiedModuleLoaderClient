ca3c041082ce88587c2c2d7675b5ae78a8464a0d
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-11-15 13:17:19 +0100

    feat: add new method stripEscapeSequencesColor() and use it in stripEscapeSequences() for getTextLength()

--- src/Classes/Cli/TextRenderer.php
+++ src/Classes/Cli/TextRenderer.php
@@ -34,6 +34,18 @@ class TextRenderer
         return "\e[" . $color . "m" . $text . "\e[0m";
     }
 
+    public static function stripEscapeSequencesColor(string $text): string
+    {
+        // Muster für Escape-Sequenzen finden und ersetzen
+        $pattern = '/\\e\[\d+m(.*?)\\e\[0m/';
+        $replacement = '$1';
+
+        // Escape-Sequenzen filtern
+        $filteredString = preg_replace($pattern, $replacement, $text);
+
+        return $filteredString;
+    }
+
     public static function stripEscapeSequenceLink(string $text): string
     {
         // Muster für Escape-Sequenzen finden und ersetzen
@@ -48,13 +60,14 @@ class TextRenderer
 
     public static function stripEscapeSequences(string $text): string
     {
-        return self::stripEscapeSequenceLink($text);
+        $strippedText = self::stripEscapeSequencesColor($text);
+        return self::stripEscapeSequenceLink($strippedText);
     }
 
     public static function getTextLength(string $text): int
     {
-        $testStriped = self::stripEscapeSequences($text);
-        $textLength = strlen($testStriped);
+        $strippedText = self::stripEscapeSequences($text);
+        $textLength = strlen($strippedText);
         return $textLength;
     }
 
