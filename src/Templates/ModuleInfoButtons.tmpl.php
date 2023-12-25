22369368f83cad0d8b601115de168ca4421cc280
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-12-25 22:14:06 +0100

    feat: use new class ButtonViewModel

--- src/Templates/ModuleInfoButtons.tmpl.php
+++ src/Templates/ModuleInfoButtons.tmpl.php
@@ -7,110 +7,99 @@
 defined('LOADED_FROM_INDEX') && LOADED_FROM_INDEX ?? die('Access denied.');
 
 use RobinTheHood\ModifiedModuleLoaderClient\Config;
+use RobinTheHood\ModifiedModuleLoaderClient\ViewModels\ModuleViewModel;
+use RobinTheHood\ModifiedModuleLoaderClient\ViewModels\ButtonViewModel as Button;
+
+/** @var ModuleViewModel $moduleView */
+
+$buttonInfo = Button::create(Button::PRIMARY)
+    ->addAction('Zur installierten Version', $moduleView->getInstalledUrl('moduleInfo'));
+
+$buttonPullAndInstall = Button::create(Button::PRIMARY)
+    ->addAction('Download & Installieren', $moduleView->getLoadAndInstallUrl('moduleInfo'))
+    ->addAction('Download', $moduleView->getLoadModuleUrl('moduleInfo'));
+
+$buttonPull = Button::create(Button::PRIMARY)
+    ->addAction('Download', $moduleView->getLoadModuleUrl('moduleInfo'));
+
+$buttonInstall = Button::create(Button::SUCCESS)
+    ->addAction('Installieren', $moduleView->getInstallUrl('moduleInfo'))
+    //->addAction('Installieren ohne Abhängigkeiten', '#')
+    ->addAction('Installieren ohne Abhängigkeiten erzwingen', $moduleView->getForceInstallUrl('moduleInfo'));
+
+$buttonUpdate = Button::create(Button::SUCCESS)
+    ->addAction('Update installieren', $moduleView->getUpdateUrl('moduleInfo'))
+    ->addAction('Update installieren ohne Abhängigkeiten erzwingen', $moduleView->getForceUpdateUrl('moduleInfo'));
+
+$buttonDiscard = Button::create(Button::DANGER)
+    ->addConfirmAction(
+        'Änderungen verwerfen',
+        $moduleView->getRevertChangesUrl('moduleInfo'),
+        'Möchtest du deine Änderungen wirklich rückgängig machen?'
+    )
+    ->addConfirmAction(
+        'Änderungen inkl. Templates verwerfen',
+        $moduleView->getRevertChangesWithTemplateUrl('moduleInfo'),
+        'Möchtest du deine Änderungen wirklich rückgängig machen?'
+    );
+
+$buttonDiscardLink = Button::create(Button::WARNING)
+    ->addAction(
+        'Änderungen übernehmen',
+        $moduleView->getRevertChangesUrl('moduleInfo')
+    )
+    ->addAction(
+        'Änderungen inkl. Templates übernehmen',
+        $moduleView->getRevertChangesWithTemplateUrl('moduleInfo')
+    );
+
+$buttonUninstall = Button::create(Button::DANGER)
+    ->addAction('Deinstallieren', $moduleView->getUninstallUrl('moduleInfo'))
+    ->addAction('Deinstallieren erzwingen', $moduleView->getForceUninstallUrl('moduleInfo'));
+
+$buttonDelete = Button::create(Button::DANGER)
+    ->addConfirmAction('Löschen', $moduleView->getUnloadModuleUrl('moduleInfo'), 'Möchtest du das Modul wirklich löschen?');
+
+$jsGoToFilesTab = "
+    <script>
+        $(document).ready(function() {
+            $('#v-pills-files-tab').tab('show');
+        });
+    </script>
+";
 ?>
 
-
 <div class="moduleinfo-buttons">
+    <?php
+    if ($moduleView->isUpdatable() && !$moduleView->isRepairable()) {
+        echo $buttonUpdate;
+    } elseif ($moduleView->hasInstalledVersion()) {
+        echo $buttonInfo;
+    }
 
-    <div class="btn-group">
-        <button type="button" class="btn btn-outline-success">Download & Installieren</button>
-        <button type="button" class="btn btn-outline-success dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-expanded="false">
-            <span class="sr-only">Toggle Dropdown</span>
-        </button>
-        <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Download & Installieren</a>
-            <a class="dropdown-item" href="#">Download</a>
-        </div>
-    </div>
+    if ($moduleView->isRepairable()) {
+        if (Config::getInstallMode() != 'link') {
+            echo $buttonDiscard;
+        } else {
+            echo $buttonDiscardLink;
+            echo $jsGoToFilesTab;
+        }
+    }
 
-    <div class="btn-group">
-        <button type="button" class="btn btn-outline-success">Installieren</button>
-        <button type="button" class="btn btn-outline-success dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-expanded="false">
-            <span class="sr-only">Toggle Dropdown</span>
-        </button>
-        <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Installieren</a>
-            <a class="dropdown-item" href="#">Installieren ohne Abhängigkeiten</a>
-            <a class="dropdown-item" href="#">Installieren ohne Abhängigkeiten erzwingen</a>
-        </div>
-    </div>
+    if ($moduleView->isCompatibleLoadableAndInstallable()) {
+        echo $buttonPullAndInstall;
+    } elseif ($moduleView->isIncompatibleLoadebale()) {
+        echo $buttonPull;
+    } elseif ($moduleView->isUninstallable() && !$moduleView->isRepairable()) {
+        echo $buttonUninstall;
+    } elseif ($moduleView->isCompatibleInstallable()) {
+        echo $buttonInstall;
+    } elseif ($moduleView->isIncompatibleInstallable()) {
+        echo $buttonInstall;
+    }
 
-    <div class="btn-group">
-        <button type="button" class="btn btn-outline-danger">Deinstallieren</button>
-        <button type="button" class="btn btn-outline-danger dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-expanded="false">
-            <span class="sr-only">Toggle Dropdown</span>
-        </button>
-        <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Deinstallieren</a>
-            <a class="dropdown-item" href="#">Deinstallieren erzwingen</a>
-        </div>
-    </div>
-
-    <div class="btn-group">
-        <button type="button" class="btn btn-outline-danger">Änderungen verwerfen</button>
-        <button type="button" class="btn btn-outline-danger dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-expanded="false">
-            <span class="sr-only">Toggle Dropdown</span>
-        </button>
-        <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Änderungen verwerfen</a>
-            <a class="dropdown-item" href="#">Änderungen inkl. Templates verwerfen</a>
-        </div>
-    </div>
-
-
-
-    <?php if ($moduleView->isUpdatable() && !$moduleView->isRepairable()) { ?>
-        <a class="button button-success" href="<?= $moduleView->getUpdateUrl('moduleInfo') ?>">Update installieren</a>
-    <?php } ?>
-
-    <?php if ($moduleView->isRepairable()) { ?>
-
-        <a class="button button-danger" onclick="return confirm('Möchtest du deine Änderungen wirklich rückgängig machen?');" href="<?= $moduleView->getRevertChangesUrl('moduleInfo') ?> ">
-            <?php if (Config::getInstallMode() != 'link') {?>
-                <i class="fas fa-tools fa-fw"></i>
-                Änderungen verwerfen
-            <?php } else { ?>
-                <i class="fas fa-check fa-fw"></i>
-                Änderungen übernehmen (Link-Mode)
-                <script>
-                    $(document).ready(function() {
-                        $('#v-pills-files-tab').tab('show');
-                    });
-                </script>
-            <?php } ?>
-        </a>
-
-        <a class="button button-danger" onclick="return confirm('Möchtest du deine Änderungen wirklich rückgängig machen?');" href="<?= $moduleView->getRevertChangesWithTemplateUrl('moduleInfo') ?> ">
-            <?php if (Config::getInstallMode() != 'link') {?>
-                <i class="fas fa-tools fa-fw"></i>
-                Änderungen verwerfen inkl. Templates
-            <?php } else { ?>
-                <i class="fas fa-check fa-fw"></i>
-                Änderungen übernehmen inkl. Templates (Link-Mode)
-            <?php } ?>
-        </a>
-    <?php } ?>
-
-    <?php if ($moduleView->isCompatibleLoadableAndInstallable()) { ?>
-        <a class="button button-default" href="<?= $moduleView->getLoadAndInstallUrl('moduleInfo') ?>">Download & Install</a>
-
-    <?php } elseif ($moduleView->isIncompatibleLoadebale()) { ?>
-        <a class="button button-default" href="<?= $moduleView->getLoadModuleUrl('moduleInfo') ?>">Download (inkompatible Version)</a>
-
-    <?php } elseif ($moduleView->isUninstallable() && !$moduleView->isRepairable()) { ?>
-        <a class="button button-danger" href="<?= $moduleView->getUninstallUrl('moduleInfo') ?>">Deinstallieren</a>
-
-    <?php } elseif ($moduleView->isCompatibleInstallable()) { ?>
-        <a class="button button-success" href="<?= $moduleView->getInstallUrl('moduleInfo') ?>">Installieren</a>
-
-    <?php } elseif ($moduleView->isIncompatibleInstallable()) { ?>
-        <a class="button button-success" href="<?= $moduleView->getForceInstallUrl('moduleInfo') ?>">Installieren (inkompatible Version)</a>
-
-    <?php } elseif ($moduleView->hasInstalledVersion()) { ?>
-        <a class="button button-default" href="<?= $moduleView->getInstalledUrl('moduleInfo') ?>">Zur installierten Version</a>
-    <?php } ?>
-
-    <?php if (!$moduleView->isRemote() && $moduleView->isLoaded() && !$moduleView->isInstalled()) { ?>
-        <a class="button button-danger" onclick="return confirm('Möchtest du das Modul wirklich entfernen?');" href="<?= $moduleView->getUnloadModuleUrl('moduleInfo') ?>">Modul löschen</a>
-    <?php } ?>
+    if (!$moduleView->isRemote() && $moduleView->isLoaded() && !$moduleView->isInstalled()) {
+        echo $buttonDelete;
+    }
+    ?>
 </div>
